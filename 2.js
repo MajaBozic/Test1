4af8ecdd3733f3d982902e4e4598f3e9fd1ac127
MajaBozic <MajaBozic@outlook.com>
2020-08-05 20:39:20 +0200

    izmena drugog zadatka

--- 2.js
+++ 2.js
@@ -1,20 +1,20 @@
 // Program za ispisivanje dijamanta sa rupom za uneto N
 
-{
-    let N = 5     //6
 
-    for (let i = 1; i <= N - 1; i++){
-        let red = ''
-        red += ' '.repeat(N - i) +'*'.repeat(2 * i - 1)
+let N = 5     //6
+
+for (let i = 1; i <= N - 1; i++){
+    let red = ''
+    red += ' '.repeat(N - i) +'*'.repeat(2 * i - 1)
         console.log(red)
-    }
-    let srednjRed = '*'.repeat(N - 1) + ' ' + '*'.repeat(N - 1)
+}
+
+let srednjRed = '*'.repeat(N - 1) + ' ' + '*'.repeat(N - 1)
     console.log(srednjRed)
 
-    for (let j = 1; j <= N - 1; j++){
-        let str = ''
-        str += ' '.repeat(j) + '*'.repeat(N - j) + '*'.repeat(N - j - 1)
-        console.log(str)
-    }
+for (let j = 1; j <= N - 1; j++){
+    let red2 = ''
+    red2 += ' '.repeat(j) + '*'.repeat(N - j) + '*'.repeat(N - j - 1)
+        console.log(red2)
 }
 
