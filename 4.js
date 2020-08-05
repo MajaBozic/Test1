b1b19f051e8802e50cebe79c451df7750cd1176f
MajaBozic <MajaBozic@outlook.com>
2020-08-05 21:56:24 +0200

    izmenjen cetvrti zadatak

--- 4.js
+++ 4.js
@@ -1,8 +1,15 @@
 // Napisati program koji ispisuje tablicu mnozenja za 1 cifru
 
-let blank = 1, 2, 3, 4, 5, 6, 7, 8, 9
-let cifra = 5
-let mnznj = cifra, (cifra * 2), (cifra * 3), (cifra * 4), (cifra * 5), (cifra * 6), (cifra * 7), (cifra * 8),  (cifra * 9)
+let br = 5
+let drugiRed = '5 x i = '
 
-console.log(blank)
-console.log(mnznj)
+console.log('  i   = 1  2  3  4  5  6  7  8  9')
+for (i = 1; i <= 9; i ++){
+   if (i * br < 10) {
+      drugiRed += i * br + '  '
+   }
+   else {
+      drugiRed += i * br + ' '
+   }
+}
+console.log(drugiRed)
